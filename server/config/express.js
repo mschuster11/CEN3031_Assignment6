7f5cf85a979f1c48b5117a1501ce08c3e73458ac
Mark Schuster <mschuster11@ufl.edu>
2017-09-21 18:50:26 -0400

    Updated for heroku envs

--- server/config/express.js
+++ server/config/express.js
@@ -3,13 +3,12 @@ var path = require('path'),
     mongoose = require('mongoose'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
-    config = require('./config'),
     listingsRouter = require('../routes/listings.server.routes'), 
     getCoordinates = require('../controllers/coordinates.server.controller.js');
 
 module.exports.init = function() {
   //connect to database
-  mongoose.connect(config.db.uri);
+  mongoose.connect(process.env.DB_URL);
 
   //initialize app
   var app = express();
