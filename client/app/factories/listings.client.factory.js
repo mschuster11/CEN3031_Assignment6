5e782ba29184bb907a2b4aa2d8fbb0a352a49577
Mark Schuster <mschuster11@ufl.edu>
2017-09-21 22:30:33 -0400

    changed links

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($https) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/listings');
+        return $https.get('https://stark-savannah-38279.herokuapp.com/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
+        return $https.post('https://stark-savannah-38279.herokuapp.com/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
+        return $https.get('https://stark-savannah-38279.herokuapp.com/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
+        return $https.put('https://stark-savannah-38279.herokuapp.com/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $https.delete('https://stark-savannah-38279.herokuapp.com/api/listings/' + id);
       }
     };
 
